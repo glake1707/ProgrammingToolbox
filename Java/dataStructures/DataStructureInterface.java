0bcd73bd75700fbe73874b3bbd31005abbb4a31b
glake1707 <glake@purdue.edu>
2017-12-29 19:16:52 -0500

    12/29/2017

--- Java/dataStructures/DataStructureInterface.java
+++ Java/dataStructures/DataStructureInterface.java
@@ -5,12 +5,18 @@ public interface DataStructureInterface {
     //use java generic types to make it work on objects and primitive types
     //may have to use BigInteger and others to make primitive types into objects
 
-    //public void append("learn java generic types");
-    //public void insert(Object a);
-    //public void prepend(Object a);
-    //public type? getValue(int index);
-    //public void delete(int index);
-    //public void delete(Object a); //deletes all instances of specified object
-    //public void clearArray();
-    //public void clearBetween(int a, int b);
+    //public void append(E e); //appends the object to the data structure
+    //public void insert(int index, E e); inserts the object at index
+    //public void prepend(E e); //prepends the object to the data structure
+    //public E getValue(int index); //returns the object at the specified index
+    //public void deleteByIndex(int index); deletes the object at index
+    //public boolean delete(E e); //deletes all instances of specified object, returns false if no instances exist
+    //public void clear(); //clear the contents and resizes the structure to a default size
+    //public <T> T[] toArray(T[] t); returns an array containing the objects in the structure
+    //public int indexOf(E element); // returns the index of the first element
+    //public int[] indexOf(E element); //returns the indices of all elements in an int array
+    //public void clearBetween(int a, int b); //deletes the section between a and b inclusive
+    // public boolean contains(Object o); //returns true if the element exists in the structure
+    //public boolean isEmpty(); //returns true if the structure is empty
+    //public int getSize(); //returns the number of elements in the data structure
 }
--- Java/dataStructures/DynamicArray/DynamicArray.java
+++ Java/dataStructures/DynamicArray/DynamicArray.java
@@ -1,6 +1,20 @@
 /**
  * Created by gabriel on 12/27/17.
  */
-public class DynamicArray implements DataStructureInterface {
+public class DynamicArray<E> implements DataStructureInterface {
+    private Object[] array;
+    private int size;
+
+    /*
+        to instantiate: DynamicArray<E> array = new DynamicArray<E>(size);
+     */
+    public DynamicArray(int size) throws IllegalArgumentException {
+        this.size = size;
+        if (size < 0) {
+            throw new IllegalArgumentException();
+        }
+        this.array = new Object[size];
+    }
+
 
 }
