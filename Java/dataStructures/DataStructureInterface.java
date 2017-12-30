17b6217a5085f28beefb7633e784e4e1b38f8085
glake1707 <glake@purdue.edu>
2017-12-29 22:28:35 -0500

    12/29/2017

--- Java/dataStructures/DataStructureInterface.java
+++ Java/dataStructures/DataStructureInterface.java
@@ -1,11 +1,11 @@
 /**
  * Created by gabriel on 7/23/17.
  */
-public interface DataStructureInterface {
+public interface DataStructureInterface<E> {
     //use java generic types to make it work on objects and primitive types
-    //may have to use BigInteger and others to make primitive types into objects
+    //may have to use BigInteger and others to make primitive types into object
 
-    //public void append(E e); //appends the object to the data structure
+    public void append(E e); //appends the object to the data structure
     //public void insert(int index, E e); inserts the object at index
     //public void prepend(E e); //prepends the object to the data structure
     //public E getValue(int index); //returns the object at the specified index
@@ -13,10 +13,10 @@ public interface DataStructureInterface {
     //public boolean delete(E e); //deletes all instances of specified object, returns false if no instances exist
     //public void clear(); //clear the contents and resizes the structure to a default size
     //public <T> T[] toArray(T[] t); returns an array containing the objects in the structure
-    //public int indexOf(E element); // returns the index of the first element
-    //public int[] indexOf(E element); //returns the indices of all elements in an int array
+    public int indexOf(E e); // returns the index of the first element
+    public int[] indexOfs(E e); //returns the indices of all elements in an int array
     //public void clearBetween(int a, int b); //deletes the section between a and b inclusive
-    // public boolean contains(Object o); //returns true if the element exists in the structure
-    //public boolean isEmpty(); //returns true if the structure is empty
-    //public int getSize(); //returns the number of elements in the data structure
+    public boolean contains(Object o); //returns true if the element exists in the structure
+    public boolean isEmpty(); //returns true if the structure is empty
+    public int getSize(); //returns the number of elements in the data structure
 }
--- Java/dataStructures/DynamicArray/DynamicArray.java
+++ Java/dataStructures/DynamicArray/DynamicArray.java
@@ -1,7 +1,7 @@
 /**
  * Created by gabriel on 12/27/17.
  */
-public class DynamicArray<E> implements DataStructureInterface {
+public class DynamicArray<E> implements DataStructureInterface<E> {
     private Object[] array;
     private int size;
 
@@ -16,5 +16,68 @@ public class DynamicArray<E> implements DataStructureInterface {
         this.array = new Object[size];
     }
 
+        public int getSize() {
+            return this.size;
+        }
 
+        public boolean isEmpty() {
+            if (this.size == 0) return false;
+            return true;
+        }
+
+        public boolean contains(Object o) {
+            for (int i = 0; i < array.length; i++) {
+                if(array[i].equals(o)) return true;
+            }
+            return false;
+        }
+
+        public int indexOf(E e) {
+            for (int i = 0; i < this.size; i++) {
+                if (array[i].equals(e)) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public int[] indexOfs(E e) {
+            int size = 0;
+            int[] indices = new int[size];
+            for (int i = 0; i < array.length; i++) {
+                if (array[i].equals(e)) {
+                    indices[++size] = i;
+                }
+            }
+            return indices;
+        }
+
+        public void resizeArray() {
+            if (this.size == array.length) {
+                Object[] temp = array;
+                array = new Object[2 * this.size];
+                for (int i = 0; i < this.size; i++) {
+                    array[i] = temp[i];
+                }
+            } else if (this.size < array.length / 3) {
+                Object[] temp = array;
+                array = new Object[this.size / 3];
+                for (int i = 0; i < this.size; i++) {
+                    array[i] = temp[i];
+                }
+            }
+        }
+
+        public void append(E e) {
+            if (this.size < array.length) {
+                array[this.size] = e;
+            } else {
+                resizeArray();
+            }
+        }
+
+        public void prepend(E e) {
+            if (this.size < array.length) {
+
+            }
+        }
 }
