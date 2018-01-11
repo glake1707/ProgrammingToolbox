6d71f3eac8719fc81363a70f7ba652e1bbfc2d4f
glake1707 <glake@purdue.edu>
2018-01-10 21:38:15 -0500

    1/10/18

--- Java/dataStructures/single_linked_list/LinkedList.java
+++ Java/dataStructures/single_linked_list/LinkedList.java
@@ -8,31 +8,96 @@ public class LinkedList<E> {
         public Node next;
     }
 
-    private Node head = null;
-    private int size = 0;
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
 
     public void clear() {
         head = null;
         size = 0;
     }
 
+    public void printList() {
+        Node t = head;
+        System.out.print("{ ");
+        while (t != null) {
+            System.out.print(t.value + " ");
+            t = t.next;
+        }
+        System.out.print("}");
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
     public void append(E e) {
-        Node<E> temp = new Node<E>();
+        Node<E> temp = new Node<>();
         temp.value = e;
         temp.next = null;
         if (head == null) {
             head = temp;
             this.size++;
-        } else {
-            Node t = head.next;
-            while (t != null ) {
+        } else  {
+            Node t = head;
+            while (t.next != null ) {
                 t = t.next;
             }
-            t = temp;
+            t.next = temp;
             this.size++;
         }
     }
     public void prepend(E e) {
+        Node<E> temp = new Node<E>();
+        temp.value = e;
+        temp.next = head;
+        this.size++;
+    }
 
+    public void deleteLast() {
+        if (head == null) {
+            return;
+        } else if (head.next == null) {
+            head = null;
+        }
+        Node t = head.next;
+        while(t.next != null) {
+            t = t.next;
+        }
+        t.next = null;
+        this.size--;
+    }
+
+    public boolean contains(Object o) {
+        Node t = head;
+        while(t !=  null) {
+            if (t.value.equals(o)) {
+                return true;
+            }
+            t = t.next;
+        }
+        return false;
+    }
+
+    public E getFirst() {
+        return (E)head.value;
+    }
+
+    public E getLast() {
+        if (head == null) {
+            return null;
+        } else if (head.next == null) {
+            return (E) head.value;
+        } else {
+            Node t = head.next;
+            while (t.next != null) {
+                t = t.next;
+            }
+            return (E)t.value;
+        }
     }
 }
--- Java/dataStructures/single_linked_list/LinkedListTest.java
+++ Java/dataStructures/single_linked_list/LinkedListTest.java
@@ -1,5 +1,37 @@
 /**
  * Created by gabriel on 1/8/18.
  */
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class LinkedListTest {
+    private LinkedList<String> list;
+
+    @Before
+    public void setup() {
+        list = new LinkedList<String>();
+    }
+
+    @Test (timeout = 1000)
+    public void testAppend() {
+        list.clear();
+        list.append("0");
+        assertTrue(list.contains("0"));
+        assertTrue(list.getLast().equals("0"));
+        assertTrue(list.getSize() == 1);
+
+        list.clear();
+
+        for (int i = 0; i < 10; i++) {
+            list.append("a" + i);
+        }
+
+        list.printList();
+        assertTrue(list.getSize() == 10);
+        assertTrue(list.getFirst().equals("a0"));
+        assertTrue(list.getLast().equals("a9"));
+
+
+    }
+
 }
